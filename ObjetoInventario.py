7e7aed1faae1d610fc09bb78656ccc3a09595c23
dalvsua-maker <dalvsua@aulaestudio.es>
2026-02-13 17:42:56 +0100

    creando OBJinventario

---
+++ ObjetoInventario.py
@@ -0,0 +1,20 @@
+import json
+
+class ObjetoInventario:
+    {
+"nombre": "Poción de energía",
+"categoria": "recuperacion",
+"contenedor": "frasco",
+"usos": 2,
+"elemento": "electricidad",
+"energia": 12
+},
+    def __init__(self,nombre,categoria,contenedor,usos,energia,elemento=None):
+        self.nombre=nombre
+        self.categoria=categoria
+        self.contenedor=contenedor
+        self.usos=usos
+        self.elemento=elemento
+        energia=energia
+
+    
