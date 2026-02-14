269105b5758829c0fcf2d47504d058aef67d2297
dalvsua-maker <dalvsua@aulaestudio.es>
2026-02-15 00:22:20 +0100

    explicacion ramas

--- ObjetoInventario.py
+++ ObjetoInventario.py
@@ -1,14 +1,7 @@
 import json
 
 class ObjetoInventario:
-    {
-"nombre": "Poción de energía",
-"categoria": "recuperacion",
-"contenedor": "frasco",
-"usos": 2,
-"elemento": "electricidad",
-"energia": 12
-},
+
     def __init__(self,nombre,categoria,contenedor,usos,energia,elemento=None):
         self.nombre=nombre
         self.categoria=categoria
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ inv.mostrar()
 categoria, objetos = inv.estrategiaSobrecarga()
 print(f"Categoría ganadora: {categoria}")
 print(f"Objetos: {objetos}")
+#Esta rama utiliza objetos para manipular la informacion 
 
 
 
